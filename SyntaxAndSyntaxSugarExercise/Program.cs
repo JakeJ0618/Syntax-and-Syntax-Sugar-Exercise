531cd13fa46188f987a581adf72b4d47a380db1f
Jake Johnston <jakejohnston618@gmail.com>
2023-03-09 11:46:29 -0600

    changed to inferred , string, and ternary

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            //explicit typing
+            //string response;//explicit typing
+           
+            //if (answer < 9)
+            //{
+                //response = answer + " is less than nine";
+            //}
+            //else
+            //{
+                //response = answer + " is greater than or equal to nine";
+            //}
+            int answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nice" : $" {answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
         }
     }
 }
